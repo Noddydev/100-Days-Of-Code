59b971cfd3bd9ccc559e0cf09accf0f8cb6c8976
Dev Kumar Kori <53847873+Noddydev@users.noreply.github.com>
2020-07-13 20:19:36 +0530

    Add files via upload

---
+++ BRReadLines.java
@@ -0,0 +1,18 @@
+// Read a String from cosloe using a BufferedReader.
+import java.io.*;
+
+class BRReadLines {
+	public static void main(String args[]) throws IOException
+	{
+		// create a BufferedReader using System.in
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String str;
+		System.out.println("Enter lies of text.");
+		System.out.println("Enter 'stop' to quit.");
+		do {
+			str = br.readLine();
+			System.out.println(str);
+		} while(!str.equals("stop"));
+	}
+}
---
+++ WriteDemo.java
@@ -0,0 +1,10 @@
+// Demonstrtae System.out.write().
+class WriteDemo {
+	public static void main(String args[]) {
+		int b;
+
+		b = 'A';
+		System.out.write(b);
+		System.out.write('\n');
+	}
+}
